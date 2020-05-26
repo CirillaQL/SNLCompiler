96827c2696ca6f82511c193baeb92424f85855b4
CirillaQL <1194946223@qq.com>
2020-05-26 19:51:21 +0800

    完成FileReader

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
-#include <fstream>
-#include <string>
-#include <iostream>
+#include "Scanner/FileReader.hpp"
+
 using namespace std;
 
 int main()
 {
-    ifstream in("1.txt");
-    string filename;
-    string line;
-
-    if(in) // 有该文件
-    {
-        while (getline (in, line)) // line中不包括每行的换行符
-        {
-            cout << line << endl;
-        }
-    }
-    else // 没有该文件
-    {
-        cout <<"no such file" << endl;
-    }
-
+    FileReader file = FileReader();
+    file.ReadFile("1.txt");
     return 0;
 }
