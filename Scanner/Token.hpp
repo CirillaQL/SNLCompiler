41ea41a4ea908f0e48c95c4a43249aad2140bb0f
CirillaQL <1194946223@qq.com>
2020-05-27 20:55:06 +0800

    添加Token序列的实现

--- Scanner/Token.hpp
+++ Scanner/Token.hpp
@@ -6,9 +6,10 @@
 #define SNLCOMPILER_TOKEN_HPP
 
 #include <string>
-#include <utility>
+#include <vector>
 #include "Word.hpp"
 
+//Token结构
 class Token{
 private:
     Word Type;
@@ -16,6 +17,7 @@ private:
     unsigned short int line;
 public:
     Token(Word type, std::string _context, unsigned short int _line);
+    ~Token();
 };
 
 Token::Token(Word type, std::string _context, unsigned short int _line) {
@@ -23,5 +25,38 @@ Token::Token(Word type, std::string _context, unsigned short int _line) {
     this->context = _context;
     this->line = _line;
 }
+
+Token::~Token() = default;
+
+//Token序列，保存从文本文档中读取到的Token
+class TokenList{
+private:
+    std::vector<Token> tokenList;
+public:
+    TokenList();
+
+    const std::vector<Token> &getTokenList() const;
+
+    void setTokenList(const std::vector<Token> &tokenList);
+
+    ~TokenList();
+    void AddToken(Token _Token);
+};
+
+void TokenList::AddToken(Token _Token) {
+    this->tokenList.push_back(_Token);
+}
+
+const std::vector<Token> &TokenList::getTokenList() const {
+    return tokenList;
+}
+
+void TokenList::setTokenList(const std::vector<Token> &tokenList) {
+    TokenList::tokenList = tokenList;
+}
+
+TokenList::~TokenList() = default;
+
+TokenList::TokenList() = default;
 
 #endif //SNLCOMPILER_TOKEN_HPP
