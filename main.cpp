69062d17e93a1eeb07b013c617464e78159179e9
CirillaQL <1194946223@qq.com>
2020-05-28 08:36:50 +0800

    添加Release，主函数测试

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "Scanner/FileReader.hpp"
 
+#include <iostream>
+
 using namespace std;
 
 int main()
 {
     FileReader file = FileReader();
     file.ReadFile("1.txt");
+    for(auto p : file.getSrcLine()){
+        cout << p << endl;
+    }
     return 0;
 }
